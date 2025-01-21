499a08c89b1efb0ff08f556bf89a7cb13238c55a
erodozer <ero@erodozer.moe>
2025-01-20 19:57:47 -0500

    fix keypad num

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ cargo build --release
 
 cd $WORKDIR
 
-mv $WORKDIR/rust/target/debug/libgodot_keylogger.so $WORKDIR/godot/addons/keylogger/libgodot_keylogger.debug.so
-mv $WORKDIR/rust/target/release/libgodot_keylogger.so $WORKDIR/godot/addons/keylogger/libgodot_keylogger.release.so
+mv $WORKDIR/rust/target/debug/libgodot_keylogger.so $WORKDIR/godot/addons/keylogger/lib/libgodot_keylogger.debug.so
+mv $WORKDIR/rust/target/release/libgodot_keylogger.so $WORKDIR/godot/addons/keylogger/lib/libgodot_keylogger.release.so
