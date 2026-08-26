9602caa0de9f1bbdd566bb9f40c72ff7c8bd3176
erodozer <ero@erodozer.moe>
2026-08-26 13:00:54 -0400

    use center scaled mouse coordinates in noop handler

--- src/keylogger.rs
+++ src/keylogger.rs
@@ -1,7 +1,9 @@
+use godot::classes::{
+    DisplayServer, Input, InputEvent, InputEventKey,
+};
+use godot::global::{Key as GKey, MouseButton, inverse_lerp};
 use godot::prelude::*;
 use std::collections::HashMap;
-use godot::global::{Key as GKey, MouseButton};
-use godot::classes::{DisplayServer, InputEvent, InputEventKey, InputEventMouseButton, InputEventMouseMotion};
 
 #[derive(GodotClass)]
 #[class(init, singleton, base=Node)]
@@ -12,7 +14,7 @@ pub struct GlobalInput {
 
     pub mousestate: HashMap<godot::global::MouseButton, bool>,
     pub mouseposition: Vector2,
-    
+
     pub penpressure: f32,
 
     pub base: Base<Node>,
@@ -36,7 +38,7 @@ impl GlobalInput {
         // map godot keycode to evdev keycode
         match self.hold_keystate.get(&keycode) {
             Some(false) => self.is_key_pressed(keycode),
-            _ => false
+            _ => false,
         }
     }
 
@@ -59,42 +61,50 @@ impl GlobalInput {
 #[godot_api]
 impl INode for GlobalInput {
     fn ready(&mut self) {
-        self.mouseposition = DisplayServer::singleton().mouse_get_position().cast_float() / DisplayServer::singleton().screen_get_size().cast_float();
+        self.mouseposition = DisplayServer::singleton().mouse_get_position().cast_float()
+            / DisplayServer::singleton().screen_get_size().cast_float();
     }
 
     fn input(&mut self, event: Gd<InputEvent>) {
-      if let Ok(keyevent) = event.clone().try_cast::<InputEventKey>() {
-        let keycode = keyevent.get_keycode();
+        if let Ok(keyevent) = event.clone().try_cast::<InputEventKey>() {
+            let keycode = keyevent.get_keycode();
 
-        if keyevent.is_pressed() {
-          self.keystate.insert(keycode, true);
-          self.hold_keystate.insert(keycode, true);
+            if keyevent.is_pressed() {
+                self.keystate.insert(keycode, true);
+                self.hold_keystate.insert(keycode, true);
+            }
+            if keyevent.is_released() {
+                self.keystate.insert(keycode, false);
+            }
         }
-        if keyevent.is_released() {
-          self.keystate.insert(keycode, false);
-        }
-      }
-      else if let Ok(motion) = event.clone().try_cast::<InputEventMouseMotion>() {
-        self.mouseposition = motion.get_global_position();
-      }
-      else if let Ok(mouse) = event.clone().try_cast::<InputEventMouseButton>() {
-        self.mousestate.insert(mouse.get_button_index(), mouse.is_pressed());
-      }
     }
 
     fn process(&mut self, _delta: f64) {
-      for (keycode, v) in self.hold_keystate.iter_mut() {
-        let pressed = match self.keystate.get(keycode) {
-            Some(res) => *res,
-            _ => false
-        };
-        let prev_state = match self.prev_keystate.get(keycode) {
-            Some(res) => *res,
-            _ => false
-        };
+        for (keycode, v) in self.hold_keystate.iter_mut() {
+            let pressed = match self.keystate.get(keycode) {
+                Some(res) => *res,
+                _ => false,
+            };
+            let prev_state = match self.prev_keystate.get(keycode) {
+                Some(res) => *res,
+                _ => false,
+            };
 
-        *v = pressed && prev_state;
-      }
+            *v = pressed && prev_state;
+        }
+
+        let dimensions = DisplayServer::singleton().screen_get_size().cast_float();
+		let position =  DisplayServer::singleton().mouse_get_position().cast_float();
+        let screen_half = dimensions / 2.0;
+        let scaled = Vector2 {
+            x: inverse_lerp(screen_half.x as f64, dimensions.x as f64, position.x as f64) as f32,
+            y: inverse_lerp(screen_half.y as f64, dimensions.y as f64, position.y as f64) as f32,
+        };
+        self.mouseposition = scaled;
+
+		self.mousestate.insert(MouseButton::LEFT, Input::singleton().is_mouse_button_pressed(MouseButton::LEFT));
+		self.mousestate.insert(MouseButton::MIDDLE, Input::singleton().is_mouse_button_pressed(MouseButton::MIDDLE));
+		self.mousestate.insert(MouseButton::RIGHT, Input::singleton().is_mouse_button_pressed(MouseButton::RIGHT));
     }
 }
 
